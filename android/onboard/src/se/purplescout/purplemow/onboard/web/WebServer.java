cb832bbf94bbb98461591ac901edd7264f5b50e5
Lars Åslin <lars.aslin@purplescout.se>
2012-06-10 20:27:07 +0200

    fix WebServer

--- android/onboard/src/se/purplescout/purplemow/onboard/web/WebServer.java
+++ android/onboard/src/se/purplescout/purplemow/onboard/web/WebServer.java
@@ -28,30 +28,31 @@ public class WebServer extends NanoHTTPD {
 	@Override
 	public Response serve(String uri, String method, Properties header, Properties parms, Properties files) {
 		if (uri.equalsIgnoreCase("/command")) {
-			String valueString = parms.getProperty("value");
-			int value;
+
 			try {
-				value = Integer.parseInt(valueString);
+				if (parms.getProperty("cmd").equals("connect")) {
+					mainFSM.queueEvent(new MainFSMEvent(MainFSMEvent.EventType.REMOTE_CONNECTED));
+				} else if (parms.getProperty("cmd").equals("disconnect")) {
+					mainFSM.queueEvent(new MainFSMEvent(MainFSMEvent.EventType.REMOTE_DISCONNECTED));
+				} else if (parms.getProperty("cmd").equals("forward")) {
+					int value = extractInt(parms, "value");
+					motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.MOVE_FWD, value));
+				} else if (parms.getProperty("cmd").equals("left")) {
+					int value = extractInt(parms, "value");
+					motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.TURN_LEFT, value));
+				} else if (parms.getProperty("cmd").equals("stop")) {
+					motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.STOP));
+				} else if (parms.getProperty("cmd").equals("right")) {
+					int value = extractInt(parms, "value");
+					motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.TURN_RIGHT, value));
+				} else if (parms.getProperty("cmd").equals("backward")) {
+					int value = extractInt(parms, "value");
+					motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.REVERSE, value));
+				}
 			} catch (NumberFormatException e) {
 				return new Response(HTTP_BADREQUEST, MIME_PLAINTEXT, e.getMessage());
 			} catch (NullPointerException e) {
 				return new Response(HTTP_BADREQUEST, MIME_PLAINTEXT, e.getMessage());
-			}
-
-			if (parms.getProperty("cmd").equals("connect")) {
-				mainFSM.queueEvent(new MainFSMEvent(MainFSMEvent.EventType.REMOTE_CONNECTED));
-			} else if (parms.getProperty("cmd").equals("disconnect")) {
-				mainFSM.queueEvent(new MainFSMEvent(MainFSMEvent.EventType.REMOTE_DISCONNECTED));
-			} else if (parms.getProperty("cmd").equals("forward")) {
-				motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.MOVE_FWD, value));
-			} else if (parms.getProperty("cmd").equals("left")) {
-				motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.TURN_LEFT, value));
-			} else if (parms.getProperty("cmd").equals("stop")) {
-				motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.STOP, value));
-			} else if (parms.getProperty("cmd").equals("right")) {
-				motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.TURN_RIGHT, value));
-			} else if (parms.getProperty("cmd").equals("backward")) {
-				motorFSM.queueEvent(new MotorFSMEvent(MotorFSMEvent.EventType.REVERSE, value));
 			}
 
 			return new Response(HTTP_OK, MIME_PLAINTEXT, "Moving " + parms.getProperty("cmd"));
@@ -81,5 +82,10 @@ public class WebServer extends NanoHTTPD {
 			Log.e(this.getClass().getCanonicalName(), e.getMessage(), e);
 			return new Response(HTTP_NOTFOUND, MIME_PLAINTEXT, e.getMessage());
 		}
+	}
+
+	private int extractInt(Properties parms, String key) throws NumberFormatException, NullPointerException {
+		String valueString = parms.getProperty(key);
+		return Integer.parseInt(valueString);
 	}
 }
