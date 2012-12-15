53f270c979b6e36909a398cd7a48a02876c2ef14
Lars Åslin <lars.aslin@purplescout.se>
2012-12-15 20:42:16 +0100

    Onboard + Core - Merge followBWF.

--- android/core/src/se/purplescout/purplemow/core/common/Constants.java
+++ android/core/src/se/purplescout/purplemow/core/common/Constants.java
@@ -2,8 +2,11 @@ package se.purplescout.purplemow.core.common;
 
 public class Constants {
 	public static final int FULL_SPEED = 245;
+	public static final int HALF_SPEED = FULL_SPEED / 2;
 	public static final int STOP = 0;
 
 	public static final int RANGE_LIMIT = 290;
 	public static final int BWF_LIMIT = 870;
+	public static final int BATTERY_LOW = 100;
+	public static final int BATTERY_CHARGED = 900;
 }
---
+++ android/core/src/se/purplescout/purplemow/core/fsm/mower/event/BatterySensorReceiveEvent.java
@@ -0,0 +1,28 @@
+package se.purplescout.purplemow.core.fsm.mower.event;
+
+import se.purplescout.purplemow.core.bus.event.CoreEvent;
+
+public class BatterySensorReceiveEvent extends CoreEvent<BatterySensorReceiveEventHandler> {
+
+	public static final CoreEvent.Type<BatterySensorReceiveEventHandler> TYPE = new Type<BatterySensorReceiveEventHandler>();
+
+	private final int value;
+
+	public BatterySensorReceiveEvent(int value) {
+		this.value = value;
+	}
+
+	@Override
+	public void dispatch(BatterySensorReceiveEventHandler handler) {
+		handler.onBatterySensorReceived(this);
+	}
+
+	@Override
+	public CoreEvent.Type<BatterySensorReceiveEventHandler> getType() {
+		return TYPE;
+	}
+
+	public int getValue() {
+		return value;
+	}
+}
